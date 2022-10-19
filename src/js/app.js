e689abd19a0bef97a1e6cb6b76c8c09358441c06
Victor Perez <vaperezr@unal.edu.co>
2022-10-19 15:56:43 -0500

    removes header styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,2 @@
 // TODO check this import, without it the hot reload does not work
 import '../scss/main.scss'
-
-import { header } from './header'
-
-header()
--- src/js/constants.js
+++
@@ -1,5 +0,0 @@
-// HEADER
-export const BURGUER_MENU = 'burger-menu'
-export const MENU = 'menu'
-export const CLOSE = 'close'
-export const OVERLAY = 'overlay'
