163a182540fa4c81b80c5f801ddca042a2ad36ed
Juliana Moreno <moreno.juliana.653@gmail.com>
2022-11-04 18:07:08 -0500

    Se actualizo el componente special kit, se agregaron clases al h2 del componete productos y paragragh, se cambiaron los identificadores por clases y se creo una funcion para el currency en pesos colombianos

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,12 @@ import '../scss/main.scss'
 
 // Import header.js
 import { header } from './header'
+import { utils } from './utils'
 header()
+
+if (document.getElementsByClassName('precio-producto__precio')) {
+  var precios = document.getElementsByClassName('precio-producto__precio')
+  for (var i = 0; i < precios.length; i++) {
+    precios[i].textContent = utils.convertPesos(precios[i])
+  }
+}
