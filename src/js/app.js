15b2f9a2936a9c5cb17497e2043827d5a5c5eb25
Tibe Calderon <tibecvp@gmail.com>
2022-11-09 09:50:06 -0500

    Agrega librería Swiper para el carousel de imágenes de producto

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 // TODO check this import, without it the hot reload does not work
 import '../scss/main.scss'
 
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/navigation'
+
 // Import header.js
 import { header } from './header'
 header()
