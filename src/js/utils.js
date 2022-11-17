08d91d1c911c94f6e15b581c1f1c12d16c8dadb7
Tibe Calderon <tibecvp@gmail.com>
2022-11-17 00:04:34 -0500

    Agrega el template hbs y los estilos para el componente blogPost. Además una función en utils para formatear la fecha a como está en los diseños

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,3 +8,25 @@ export const utils = {
     minimumFractionDigits: 0,
   }),
 }
+
+export function convertDate() {
+  const dateP = document.getElementsByClassName('blog__fecha')
+  const date = new Date(dateP[0].textContent)
+  const meses = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ]
+  const dateFormated =
+    meses[date.getMonth()] + ' ' + date.getDate() + ' - ' + date.getFullYear()
+  dateP[0].textContent = dateFormated
+}
